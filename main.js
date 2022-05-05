c58c11fafa58de8d7e248e9ee9427c39d1b02013
Kippn <NiklasKipp@gmx.de>
2022-05-05 18:38:28 +0200

    add Functions

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-
 // variables
-let countries = [];
-let dataYear = [];
-let dataPointsCO2 = [];
 let title = "";
 let	active = d3.select(null);
 
@@ -13,6 +9,10 @@ const suggBox = searchWrapper.querySelector(".autocom-box");
 const icon = document.querySelector(".icon");
 
 // data arrays
+let countries = [];
+let dataYear = [];
+let dataPointsCO2 = [];
+let dataPointsEnergyMix = [];
 let dataPointsCO2perPerson = [];
 let dataPointsEnergy = [];
 // json file
@@ -20,8 +20,8 @@ let jsonPath = "/owid-co2-data.json";
 // windows size
 let l = -window.innerWidth*0.11.toFixed(2);
 let r = -window.innerHeight*0.03.toFixed(2);
-let w = window.innerWidth*0.95.toFixed(2);
-let h = window.innerHeight*0.95.toFixed(2);
+let w = window.innerWidth*0.8.toFixed(2);
+let h = window.innerHeight*0.9.toFixed(2);
 let hString = '70%';
 let wString = '70%';
 let svg;
@@ -191,7 +191,6 @@ function zoomToClick(i) {
 	svg.on("start",stopped,true);
 
 	path.attr("class","feature");
-	//path.on("click",clicked);
 	path.dispatch("start",clicked);
 	d.call("start");
 	//svg.call(svg,zoom);
@@ -200,8 +199,6 @@ function zoomToClick(i) {
 		if (active.node() === this) return reset(svg,zoom);
 		active.classed("active", false);
 		active = d3.select(this).classed("active", true);
-		console.log(active);
-
 		let dx = x1,
       	dy = y1,
       	x = (x0 + x1+x0) / 2,
@@ -213,6 +210,7 @@ function zoomToClick(i) {
 		.duration(750)
 		.call( zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale) );
 		getData(title);
+
 	}
 }
 
@@ -237,11 +235,34 @@ function readTextFile(file, callback) {
 	rawFile.send(null);
 }
 
+function readCSV(file, title, _callback) {
+	d3.csv(file, function(data) {
+		for(i = 0; i < data.length; i+=56) {
+			if(data[i].Entity === title) {
+				i+= 55;
+				dataPointsEnergyMix.push({y: Number(data[i].BiofuelsConsumption).toFixed(2), name: 'Biofuels Consumption'});
+				dataPointsEnergyMix.push({y: Number(data[i].SolarConsumption).toFixed(2), name: 'Solar Consumption'});
+				dataPointsEnergyMix.push({y: Number(data[i].WindConsumption).toFixed(2), name: 'Wind Consumption'});
+				dataPointsEnergyMix.push({y: Number(data[i].HydroConsumption).toFixed(2), name: 'Hydro Consumption'});
+				dataPointsEnergyMix.push({y: Number(data[i].NuclearConsumption).toFixed(2), name: 'Nuclear Consumption'});
+				dataPointsEnergyMix.push({y: Number(data[i].GasConsumption).toFixed(2), name: 'Gas Consumption'});
+				dataPointsEnergyMix.push({y: Number(data[i].CoalConsumption).toFixed(2), name: 'Coal Consumption'});
+				dataPointsEnergyMix.push({y: Number(data[i].OilConsumption).toFixed(2), name: 'Oil Consumption'});
+				dataPointsEnergyMix.push({y: Number(data[i].GeoBiomass).toFixed(2), name: 'Geo Biomass'});
+				_callback();
+				break;
+			}
+		}
+		_callback();
+});
+}
+
 /**
  * get data per country name for charts
  * @param {*} title 
  */
  function getData(title) {
+	readCSV('/energy-consumption-by-source-and-region.csv',title,()=>{
 	readTextFile(jsonPath, function(text){
 		var data = JSON.parse(text);
 		for(let i = 0; i <data[title].data.length;i++) {
@@ -266,11 +287,13 @@ function readTextFile(file, callback) {
 		dataPointsEnergy.sort((a,b) => b.x - a.x);
 		chart();
 		chart();
-		dataPointsCO2 = [];
-		dataPointsCO2perPerson = [];
-		dataPointsEnergy = [];
 		document.getElementById("dialogBox").removeAttribute("hidden");
 	});
+});
+dataPointsCO2 = [];
+dataPointsCO2perPerson = [];
+dataPointsEnergy = [];
+dataPointsEnergyMix = [];
 }
 
 /**
@@ -369,6 +392,32 @@ function chart () {
 			dataPoints: dataPointsEnergy
 		}]
 	};
+
+	var options4 = {
+		backgroundColor: "rgb(186, 186, 186)",
+		//markerColor: "rgba(56, 56, 56, 0.964)",
+		lineThickness: 10,
+		zoomEnabled: true,
+		animationEnabled: true,
+		title: {
+			text: title + " Energy Mix"
+		},
+		legend:{
+			cursor: "pointer",
+			itemclick: explodePie
+		},
+		data: [{
+			type: "pie",
+			showInLegend: true,
+			radius: 200,
+			toolTipContent: "{name}: <strong>{y} tWh</strong>",
+			indexLabel: "{name} - {y} tWh",
+			//color: "rgba(56, 56, 56, 0.964)",
+			//lineColor: "rgb(4, 86, 4)",
+			//xValueFormatString: "Year ####",
+			dataPoints: dataPointsEnergyMix,
+		}]
+	};
 		
 		$("#dialogBox").dialog({
 			open: function(event,ui) {
@@ -393,7 +442,6 @@ function chart () {
         // Remove click handler for the current .ui-widget-overlay
         $(document).off("click",".ui-widget-overlay");
 				reset(svg,zoom);
-
         // Invoke parent close method
     },
 			hide: { effect: 'drop', duration: 250 },
@@ -411,6 +459,16 @@ function chart () {
 		$("#chartContainer1").CanvasJSChart(options1);
 		$("#chartContainer2").CanvasJSChart(options2);
 		$("#chartContainer3").CanvasJSChart(options3);
+		$("#chartContainer4").CanvasJSChart(options4);
+	}
+
+	function explodePie (e) {
+		if(typeof (e.dataSeries.dataPoints[e.dataPointIndex].exploded) === "undefined" || !e.dataSeries.dataPoints[e.dataPointIndex].exploded) {
+			e.dataSeries.dataPoints[e.dataPointIndex].exploded = true;
+		} else {
+			e.dataSeries.dataPoints[e.dataPointIndex].exploded = false;
+		}
+		e.chart.render();
 	}
 
 	
