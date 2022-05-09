f3089cefc28f751a7ac763788200a23378f6dea6
Kippn <NiklasKipp@gmx.de>
2022-05-09 13:17:48 +0200

    update

--- main.js
+++ main.js
@@ -1,7 +1,71 @@
 // variables
 let title = "";
+var title2 = "";
 let	active = d3.select(null);
-
+let compare = 0;
+var options1 = {
+	backgroundColor: "rgba(186, 186, 186, 0.1)",
+	markerColor: "rgba(56, 56, 56, 0.964)",
+	zoomEnabled: true,
+	animationEnabled: true,
+	toolTip:{
+		shared: true
+	},
+	axisY: {
+		title: "CO\u{2082} mil tons ",
+		suffix: "t",
+		minimum: 0,
+		titleFontSize: 30
+	},
+	axisX: {
+		title: "years",
+		valueFormatString: "####",
+		titleFontSize: 30
+	}
+};
+var options2 = {
+	backgroundColor: "rgba(186, 186, 186, 0.1)",
+	markerColor: "rgba(56, 56, 56, 0.964)",
+	zoomEnabled: true,
+	animationEnabled: true,
+	yValueFormatString: "####",
+	toolTip:{
+		//fontColor: "rgb(4, 86, 4)",
+		shared: true
+	},
+	axisY: {
+		title: "CO\u{2082} mil tons p.P",
+		suffix: "t",
+		minimum: 0,
+		titleFontSize: 24
+	},
+	axisX: {
+		title: "years",
+		valueFormatString: "####",
+		titleFontSize: 30
+	}
+};
+var options3 = {
+	backgroundColor: "rgba(186, 186, 186, 0.1)",
+	markerColor:"rgba(56, 56, 56, 0.964)",
+	zoomEnabled: true,
+	animationEnabled: true,
+	yValueFormatString: "####",
+	toolTip:{
+		shared: true
+	},
+	axisY: {
+		title: "tWh per year",
+		suffix: "",
+		minimum: 0,
+		titleFontSize: 30
+	},
+	axisX: {
+		title: "years",
+		valueFormatString: "####",
+		titleFontSize: 30
+	}
+};
 // searchbar variables
 const searchWrapper = document.querySelector(".container-2");
 const inputBox = document.getElementById('searchbar');
@@ -11,13 +75,16 @@ const icon = document.querySelector(".icon");
 // data arrays
 let countries = [];
 let dataYear = [];
+var dataOption1 = [];
+var dataOption2 = [];
+var dataOption3 = [];
 let dataPointsCO2 = [];
 let dataPointsEnergyMix = [];
 let dataPointsCO2perPerson = [];
 let dataPointsEnergy = [];
 // json file
-let jsonPath = "/owid-co2-data.json";
-let csvPath = '/energy-consumption-by-source-and-region.csv';
+let jsonPath = "owid-co2-data.json";
+let csvPath = 'energy-consumption-by-source-and-region.csv';
 // windows size
 let l = -window.innerWidth*0.11.toFixed(2);
 let r = -window.innerHeight*0.03.toFixed(2);
@@ -30,39 +97,8 @@ if(h>w) {
 	t = h;
 	h = w;
 	w = t;
-	//hString = '30%';
-	//wString = '60%';
 }
 
-/** 
-//var $header_top = $('.header-top');
-var $nav = $('nav');
-
-$('#fullpage').fullpage({
-	sectionSelector: '.vertical-scrolling',
-  sectionsColor: ['gray'],
-  navigation: true,
-	autoScrolling:false,
-  slidesNavigation: true,
-  controlArrows: false,
-  anchors: ['firstSection', 'secondSection'],
-  //menu: '#menu',
-
-  afterLoad: function(anchorLink, index) {
-    //$header_top.css('background', 'rgba(0, 47, 77, .3)');
-    //$nav.css('background', 'rgba(0, 47, 77, .25)');
-    if (index == 2) {
-        $('#fp-nav').hide();
-      }
-  },
-
-  onLeave: function(index, nextIndex, direction) {
-    if(index == 2) {
-      $('#fp-nav').show();
-    }
-  },
-});
-*/
 
 var controller = new ScrollMagic.Controller();
 
@@ -78,6 +114,17 @@ new ScrollMagic.Scene({
 
 $(".scroll-down").click(function() {
 	document.getElementById("reveal1").scrollIntoView({behavior: 'smooth', block:'end'});
+});
+
+$('#mapNav').click(function() {
+	document.getElementById("reveal1").scrollIntoView({behavior: 'smooth', block:'end'});
+})
+
+$("button").click(function() {
+	compare = 1;
+	title2 = title;
+	$("#dialogBox").dialog('close');
+	reset(svg,zoom);
 });
 
 function findPos(obj) {
@@ -223,7 +270,7 @@ function zoomToClick(i) {
 	let x1 = (x0 + myPathBox.width)*1.11;
 	let y1 = y0 + myPathBox.height;
 
-	const d = d3.dispatch("start");
+	const d = d3.dispatch("show");
 
 	svg = d3.select("svg");
   let path = svg.select('#'+id),
@@ -234,8 +281,8 @@ function zoomToClick(i) {
 	svg.on("start",stopped,true);
 
 	path.attr("class","feature");
-	path.dispatch("start",clicked);
-	d.call("start");
+	path.dispatch("show",clicked);
+	d.call("show");
 	//svg.call(svg,zoom);
 
 	function clicked() {
@@ -248,7 +295,6 @@ function zoomToClick(i) {
       	y = (y0 + y1) / 2,
       	scale = Math.max(1, Math.min(8, 0.8 / Math.max(dx / width, dy / height))),
       	translate = [width / 2 - scale * x, height / 2 - scale * y];
-console.log(dx);
 		svg.transition()
 		.duration(750)
 		.call(zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale));
@@ -256,7 +302,7 @@ console.log(dx);
 	}
 }
 
-$("path").mousedown(function() {
+$("path").on('click',function() {
 	zoomToClick(this);
 });
 
@@ -299,6 +345,10 @@ function readCSV(file, title, _callback) {
 });
 }
 
+function getUniqueListBy(arr, key) {
+	return [...new Map(arr.map(item => [item[key], item])).values()]
+}
+
 /**
  * get data per country name for charts
  * @param {*} title 
@@ -312,6 +362,7 @@ function readCSV(file, title, _callback) {
 			let co2 = data[title].data[i].co2;
 			let co2PerPerson = data[title].data[i].co2_per_capita;
 			let energy = data[title].data[i].primary_energy_consumption;
+		
 			if(co2!=null) {
 				dataPointsCO2.push({x: year, y: co2});
 			}
@@ -327,128 +378,88 @@ function readCSV(file, title, _callback) {
 		dataPointsCO2.sort((a,b) => b.x - a.x);
 		dataPointsCO2perPerson.sort((a,b) => b.x - a.x);
 		dataPointsEnergy.sort((a,b) => b.x - a.x);
-		chart();
+
+		dataOption1.push({
+			type:'line',
+			color: "rgb(4, 86, 4)",
+			name: title,
+			lineColor: "rgb(4, 86, 4)",
+			lineThickness: 5,
+			xValueFormatString: "Year: ####", 
+			yValueFormatString: "######.## t", 
+			dataPoints: dataPointsCO2});
+
+		dataOption2.push({
+			name: title,
+			type: "line",
+			xValueFormatString:"Year: ####",
+			yValueFormatString:"#######.## t",
+			color: "rgb(4, 86, 4)",
+			lineColor: "rgb(4, 86, 4)",
+			lineThickness: 5,
+			dataPoints: dataPointsCO2perPerson
+		});
+
+		dataOption3.push({
+			name: title,
+			xValueFormatString:"Year: ####",
+			yValueFormatString:"#######.## tWh",
+			type: "line",
+			color: "rgb(4, 86, 4)",
+			lineColor: "rgb(4, 86, 4)",
+			lineThickness: 5,
+			dataPoints: dataPointsEnergy
+		});
+
+		if(compare==0) {
+		options1['title'] = 	{
+			text: title + " CO\u{2082} per year",
+			fontSize: 40
+		};
+		options2['title'] = 	{
+			text: title + " CO\u{2082} tonnes per person",
+			fontSize: 40
+		};
+		options3['title'] = 	{
+			text: title + " Energy consumption in terrawatt-hours",
+			fontSize: 40
+		};
+	} else {
+		options1['title'] = 	{
+			text: title + " vs. " + title2 + " CO\u{2082} per year",
+			fontSize: 40
+		};
+		options2['title'] = 	{
+			text: title + " vs. " + title2  + " CO\u{2082} tonnes per person",
+			fontSize: 40
+		};
+		options3['title'] = 	{
+			text: title + " vs. " + title2  + " Energy consumption in terrawatt-hours",
+			fontSize: 40
+		};
+	}
+
+		options1['data'] = removeDuplicateObjectFromArray(dataOption1,'name');
+		options2['data'] = removeDuplicateObjectFromArray(dataOption2,'name');
+		options3['data'] = removeDuplicateObjectFromArray(dataOption3,'name');
+
 		chart();
 		document.getElementById("dialogBox").removeAttribute("hidden");
 	});
 });
-dataPointsCO2 = [];
-dataPointsCO2perPerson = [];
-dataPointsEnergy = [];
-dataPointsEnergyMix = [];
 }
 
+function removeDuplicateObjectFromArray(array, key) {
+  var check = new Set();
+  return array.filter(obj => !check.has(obj[key]) && check.add(obj[key]));
+}
 /**
  * initilize charts and tabs
  */
-function chart () {
-	var options1 = {
-		backgroundColor: "rgba(186, 186, 186, 0.1)",
-		markerColor: "rgba(56, 56, 56, 0.964)",
-		zoomEnabled: true,
-		animationEnabled: true,
-		title: {
-			text: title + " CO\u{2082} per year",
-			fontSize: 40
-		},
-		toolTip:{
-			fontColor: "rgb(4, 86, 4)",
-			content:"{x}, CO\u{2082}: {y} t"
-		},
-		axisY: {
-			title: "CO\u{2082} mil tons ",
-			suffix: "t",
-			minimum: 0,
-			titleFontSize: 30
-		},
-		axisX: {
-			title: "years",
-			valueFormatString: "####",
-			titleFontSize: 30
-		},
-		data: [{
-			type: "line",
-			color: "rgba(56, 56, 56, 0.1)",
-			lineColor: "rgb(4, 86, 4)",
-			lineThickness: 5,
-			xValueFormatString: "Year ####",
-			dataPoints: dataPointsCO2
-		}]
-	};
-
-	var options2 = {
-		backgroundColor: "rgba(186, 186, 186, 0.1)",
-		markerColor: "rgba(56, 56, 56, 0.964)",
-		zoomEnabled: true,
-		animationEnabled: true,
-		yValueFormatString: "####",
-		title: {
-			text: title + " CO\u{2082} tonnes per person",
-			fontSize: 40
-		},
-		toolTip:{
-			fontColor: "rgb(4, 86, 4)",
-			content:"{x}, CO\u{2082}: {y} t"
-		},
-		axisY: {
-			title: "CO\u{2082} mil tons p.P",
-			suffix: "t",
-			minimum: 0,
-			titleFontSize: 24
-		},
-		axisX: {
-			title: "years",
-			valueFormatString: "####",
-			titleFontSize: 30
-		},
-		data: [{
-			type: "line",
-			xValueFormatString: "Year ####",
-			color: "rgba(56, 56, 56, 0.1)",
-			lineColor: "rgb(4, 86, 4)",
-			lineThickness: 5,
-			dataPoints: dataPointsCO2perPerson
-		}]
-	};
-
-	var options3 = {
-		backgroundColor: "rgba(186, 186, 186, 0.1)",
-		markerColor:"rgba(56, 56, 56, 0.964)",
-		zoomEnabled: true,
-		animationEnabled: true,
-		yValueFormatString: "####",
-		title: {
-			text: title + " Energy consumption in terrawatt-hours",
-			fontSize: 40
-		},
-		toolTip:{
-			fontColor: "rgb(4, 86, 4)",
-			content:"{x}, tWh: {y} t"
-		},
-		axisY: {
-			title: "tWh per year",
-			suffix: "",
-			minimum: 0,
-			titleFontSize: 30
-		},
-		axisX: {
-			title: "years",
-			valueFormatString: "####",
-			titleFontSize: 30
-		},
-		data: [{
-			type: "line",
-			xValueFormatString: "Year ####",
-			color: "rgba(56, 56, 56, 0.2)",
-			lineColor: "rgb(4, 86, 4)",
-			lineThickness: 5,
-			dataPoints: dataPointsEnergy
-		}]
-	};
+ function chart () {
 
 	var options4 = {
 		backgroundColor: "rgba(186, 186, 186, 0.1)",
-		//markerColor: "rgba(56, 56, 56, 0.964)",
 		zoomEnabled: true,
 		animationEnabled: true,
 		title: {
@@ -468,21 +479,19 @@ function chart () {
 
 			toolTipContent: "{name}: <strong>{y} tWh</strong>",
 			indexLabel: "{name} - {y} tWh",
-			//color: "rgba(56, 56, 56, 0.964)",
-			//lineColor: "rgb(4, 86, 4)",
-			//xValueFormatString: "Year ####",
 			dataPoints: dataPointsEnergyMix,
 		}]
 	};
-		
 		$("#dialogBox").dialog({
 			open: function(event,ui) {
 				$(document).on('click',".ui-widget-overlay",function(e){
           //call dialog close function
-					reset(svg,zoom);
        }),
 				$("#tabs").tabs({
+					beforeLoad: function(event,ui) {
+					},
 					create: function (event, ui) {
+						event.preventDefault();
 						//Render Charts after tabs have been created.
 					},
 					activate: function (event, ui) {
@@ -492,12 +501,20 @@ function chart () {
 				});
 				$('.ui-widget-overlay').bind('click', function () {
 					$("#dialogBox").dialog('close');
+					compare = 0;
+					dataOption1 = [];
+					dataOption2 = [];
+					dataOption3 = [];
 			});
 			},
 			close: function() {
         // Remove click handler for the current .ui-widget-overlay
         $(document).off("click",".ui-widget-overlay");
 				reset(svg,zoom);
+				dataPointsCO2 = [];
+				dataPointsCO2perPerson = [];
+				dataPointsEnergy = [];
+				dataPointsEnergyMix = [];
         // Invoke parent close method
     },
 			hide: { effect: 'drop', duration: 250 },
@@ -505,19 +522,17 @@ function chart () {
 			closeOnEscape: true,
 			draggable: false,
 			resizable: false,
-			//title: title + ' statistics',
-			width: wString,
-			hight: hString,
+			width: '90%',
+			hight: '100%',
 			modal: true,
-			//closeText: '',
 			show: 500,
 		}).dialog('widget').find(".ui-dialog-title").hide();
 		$("#chartContainer1").CanvasJSChart(options1);
 		$("#chartContainer2").CanvasJSChart(options2);
 		$("#chartContainer3").CanvasJSChart(options3);
 		$("#chartContainer4").CanvasJSChart(options4);
-	}
-
+	
+};
 function explodePie (e) {
 		if(typeof (e.dataSeries.dataPoints[e.dataPointIndex].exploded) === "undefined" || !e.dataSeries.dataPoints[e.dataPointIndex].exploded) {
 			e.dataSeries.dataPoints[e.dataPointIndex].exploded = true;
